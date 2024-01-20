590526d66937dd61d7237df0bf91b9bda1aae8e4
samerr22 <laksameera5@gmail.com>
2024-01-20 17:03:56 +0530

    create middle ware for the handle the error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,14 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandle } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     
     const { username , email, password } = req.body;
 
     if(!username || !email || !password || username === '' || email === '' || password === ''){
-        return res.status(400).json({ message: 'All fields are required'});
+        
+        next(errorHandle(400, 'all fields are required'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -27,7 +29,7 @@ export const signup = async (req, res) => {
         
     } catch (error) {
 
-        res.status(500).json({ message: error.message});
+       next(error);
 
 
 
