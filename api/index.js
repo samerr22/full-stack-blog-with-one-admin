8d48f13b43449fe0744865e221bd532a0b7d639c
samerr22 <laksameera5@gmail.com>
2024-01-24 20:54:28 +0530

    add comment section

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import UserRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -28,6 +29,7 @@ app.listen(3000, () => {
 app.use('/api/user', UserRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
 
 
 app.use((err, req, res, next) =>{
