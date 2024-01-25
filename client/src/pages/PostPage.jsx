18d8f99fff3c3c8c0186c7b75052fa78e6c74b46
samerr22 <laksameera5@gmail.com>
2024-01-25 12:34:03 +0530

    add recent artical

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,9 +3,7 @@ import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
 import CommentSection from '../components/CommentSection';
-
-
-
+import PostCard from '../components/PostCard';
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -39,7 +37,18 @@ export default function PostPage() {
   }, [postSlug]);
 
   useEffect(() => {
-    
+    try {
+      const fetchRecentPosts = async () => {
+        const res = await fetch(`/api/post/getposts?limit=3`);
+        const data = await res.json();
+        if (res.ok) {
+          setRecentPosts(data.posts);
+        }
+      };
+      fetchRecentPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
   }, []);
 
   if (loading)
@@ -77,14 +86,15 @@ export default function PostPage() {
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
       <div className='max-w-4xl mx-auto w-full'>
-        <CallToAction /> 
+        <CallToAction />
       </div>
-     <CommentSection postId={post._id} /> 
+      <CommentSection postId={post._id} />
 
       <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
         <div className='flex flex-wrap gap-5 mt-5 justify-center'>
-        
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
         </div>
       </div>
     </main>
